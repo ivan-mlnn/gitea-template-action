e39e54886b396185738d9f9dbafd17b8d1d056f7
miu <miu@tsinet.ru>
2023-08-10 13:33:17 +0300

    -

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module gitea-template-action
 
 go 1.20
 
-require github.com/sethvargo/go-githubactions v1.1.0
+require (
+	code.gitea.io/sdk/gitea v0.15.1
+	github.com/sethvargo/go-githubactions v1.1.0
+)
 
-require github.com/sethvargo/go-envconfig v0.8.0 // indirect
+require (
+	github.com/hashicorp/go-version v1.2.1 // indirect
+	github.com/sethvargo/go-envconfig v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"code.gitea.io/sdk/gitea"
 	"fmt"
+	gha "github.com/sethvargo/go-githubactions"
 	"os"
+	"strings"
 )
 
 func main() {
-	//_ = gha.GetInput("username")
-	//gha.
+	ctx, err := gha.Context()
+	if err != nil {
+		gha.Fatalf("failed to get context: %v", err)
+	}
+	apiKey := os.Getenv("GITHUB_TOKEN")
+
+	c, err := gitea.NewClient(ctx.ServerURL, gitea.SetToken(apiKey))
+	if err != nil {
+		gha.Fatalf("failed to create gitea client: %v", err)
+	}
+	owner := ctx.RepositoryOwner
+	repo := strings.Split(ctx.Repository, "/")[1]
+
+	r, _, err := c.GetRepo(owner, repo)
+	if err != nil {
+		gha.Fatalf("failed to GetRepo: %v", err)
+	}
+	_ = r.Description
+	fmt.Println(r.Description)
+
+	files, err := os.ReadDir(ctx.Workspace)
+	if err != nil {
+		gha.Fatalf("failed to ReadDir: %v", err)
+	}
+
+	for _, s := range files {
+		if s.IsDir() == false {
+			fmt.Println(s.Name())
+		}
+	}
+
 	env := os.Environ()
 	for _, s := range env {
 		fmt.Println(s)
